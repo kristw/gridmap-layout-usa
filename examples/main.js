ebce87aac7e6633a5e28e81f1f20709519aa3c3f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2016-10-19 01:24:01 -0700

    show misdirection states

--- examples/main.js
+++ examples/main.js
@@ -1,10 +1,7 @@
 // var color = d3.scale.category10();
-// var color = d3.scale.linear()
-//   .domain([0, 20])
-//   .range(['#ecf0f1', '#c0392b']);
 
 var color = d3.scale.quantize()
-  .domain([0, 20])
+  .domain([0, 180])
   .range(['#f1e5db','#f5d4bc','#f7c59e','#f7b480','#f5a462','#f29443','#ed841e']);
 
 var names = {
@@ -18,20 +15,38 @@ var names = {
 
 var container = d3.select('.map-container');
 
-['nyt', 'npr', 'guardian', 'wp', '538', 'bloomberg'].forEach(function(source){
-  var block = container.append('div').classed('block', true);
-  var title = block.append('div').classed('title', true).text(names[source]);
-  var element = block.append('div').classed('map', true);
-  var map = new d3Kit.Gridmap(element[0][0], {
-    col: function(d){return +d.x;},
-    row: function(d){return +d.y;},
-    tileWidth: 21,
-    tileHeight: 21,
-    fill: function(d){return color(d.name.length);}
-  });
+d3.json('qualities.json', function(error, qualities) {
+  const misdirections = _(qualities)
+    .keyBy(d => d.source)
+    .mapValues(value => _(value.misdirections)
+      .flatMap(d => [d.region1, d.region2])
+      .uniq()
+      .keyBy(d => d)
+      .value()
+    )
+    .value();
 
-  d3.csv('dist/gridmap-layout-usa-'+source+'.csv', function(error, data){
-    map.data(data).resizeToFitMap();
+  ['nyt', 'npr', 'guardian', 'wp', '538', 'bloomberg'].forEach(function(source){
+    const sourceKey = source==='538' ? 'fivethirtyeight' : source;
+    const info = misdirections[sourceKey];
+
+    var block = container.append('div').classed('block', true);
+    var title = block.append('div').classed('title', true).text(names[source]);
+    var element = block.append('div').classed('map', true);
+    var map = new d3Kit.Gridmap(element[0][0], {
+      col: function(d){return +d.x;},
+      row: function(d){return +d.y;},
+      tileWidth: 21,
+      tileHeight: 21,
+      fill: function(d){
+        return info[d.key] ? '#ed841e' : '#f1e5db';
+        return color(d.name.length);
+      }
+    });
+
+    d3.csv('dist/gridmap-layout-usa-'+source+'.csv', function(error, data){
+      map.data(data).resizeToFitMap();
+    });
   });
 });
 
