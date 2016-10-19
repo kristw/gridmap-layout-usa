684ad0f5259016e6ee3ba3851e2db7fb87beba6b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2016-10-19 00:43:42 -0700

    latest update

--- src/quality.R
+++ src/quality.R
@@ -3,8 +3,19 @@ library('scales')
 library('RColorBrewer')
 
 data <- read.csv('output/quality-data2.csv')
-data$metric <- ordered(data$metric, levels=c('recall', 'primaryrecall', 'secondaryrecall', 'inaccuracy', 'misdirection', 'area'))
+data$metric <- ordered(data$metric, levels=c('topology', 'primary-topology', 'secondary-topology', 'inaccuracy', 'misdirection', 'area'))
 data$source <- ordered(data$source, levels=rev(c('nyt', 'wp', 'npr', 'guardian', 'fivethirtyeight', 'bloomberg')))
+
+ggplot(subset(data, metric!='area'), aes(fill=source,x=source, y=value)) +
+  facet_grid(metric ~ .) +
+  scale_fill_manual(values=c("#556270", "#4ecdc4", "#c7f464", "#ff6B6B", "#c44d58", "#EDC951")) +
+  scale_y_continuous(labels = scales::percent) +
+  geom_bar( stat = "identity") +
+  coord_flip() +
+  geom_text(aes(y=value + 0.1, label = sprintf("%1.1f%%", 100*value))) +
+  theme_bw() 
+
+ggsave('images/metrics_col.pdf')
 
 ggplot(subset(data, metric!='area'), aes(fill=source,x=source, y=value)) +
   facet_wrap(~metric) +
@@ -25,3 +36,4 @@ ggplot(subset(data, metric=='area'), aes(fill=source, x=source, y=value)) +
   theme_bw() 
 
 ggsave('images/area.png')
+ggsave('images/area.pdf')
